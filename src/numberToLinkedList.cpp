da890788497665f10a63349222b5c54e289fbb6b
Divya37 <undidivya1996@gmail.com>
2015-11-22 16:32:26 +0500

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -19,6 +19,52 @@ struct node {
 	struct node *next;
 };
 
-struct node * numberToLinkedList(int N) {
-	return NULL;
+struct node * numberToLinkedList(int N)
+{
+	int i, temp, *arr, length;
+	struct node *head, *temp_var, *lastnode;
+	lastnode = (struct node *)malloc(sizeof(struct node));
+	head = NULL;
+
+	if (N < 0)
+		N = N*-1;
+	if (N == 0)
+	{
+		head = (struct node*)malloc(sizeof(struct node));
+		head->num = N;
+		head->next = NULL;
+		return head;
+	}
+	temp = N;
+
+	for (i = 0; temp > 0; i++)
+		temp = temp / 10;
+	length = i;
+	arr = (int *)malloc(sizeof(int)*i);
+	for (i = 0; N > 0; i++)
+	{
+		arr[i] = N % 10;
+		N = N / 10;
+	}
+	temp = 0;
+	
+	for (i = length - 1; i >= 0; i--)
+	{
+		if (head == NULL)
+		{
+			temp_var = (struct node *)malloc(sizeof(struct node));
+			temp_var->num = arr[i];
+			temp_var->next = NULL;			
+			lastnode =head= temp_var;
+		}
+		else
+		{			
+			temp_var = (struct node *)malloc(sizeof(struct node));
+			temp_var->num = arr[i];
+			temp_var->next = NULL;	
+			lastnode->next = temp_var;
+			lastnode = temp_var;
+		}
+	}	
+	return head;
 }
